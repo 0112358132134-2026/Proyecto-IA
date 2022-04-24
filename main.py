038ea6681ed6f62c32d8e3b4ee20bedfb2aabc9b
0112358132134-2026 <68541@cag.edu.gt>
2022-04-24 11:13:00 -0600

    def cargar_csv(nombre_archivo):
    :)

---
+++ main.py
@@ -0,0 +1,4 @@
+import carga_csv as _csv
+
+if __name__ == "__main__":
+    _csv.cargar_csv("movie_metadata.csv")
--- peliculas.py
+++
@@ -1,9 +0,0 @@
-from sympy import resultant
-
-
-def cargar_peliculas(csv):
-    resultado = ""
-    for fila in csv:
-        if fila == "":
-            resultado = "OK"
-    return resultado
